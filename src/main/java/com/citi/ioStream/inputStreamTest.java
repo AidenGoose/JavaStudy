ca9fe3d870b6bc58f55b3423413b8c7a3ccd862c
AidenGoose <1354790768@qq.com>
2024-02-07 21:08:28 +0800

    Create inputStreamTest.java
    Add io stream sample

---
+++ src/main/java/com/citi/ioStream/inputStreamTest.java
@@ -0,0 +1,15 @@
+package com.citi.ioStream
+
+public class inputStreamTest {
+    /*
+     * Stream流， 先进先出
+     * 顺序存取：可以一个接一个往流中写入一串字节， 读出也按写入顺序读取
+     * 
+     */
+    public static void main(String[] args) {
+        
+
+    }
+
+
+}
