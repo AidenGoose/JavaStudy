8b558271be33bf67021f58418fbd80092d5f0361
AidenGoose <1354790768@qq.com>
2024-02-07 21:08:09 +0800

    Update First.java
    Add part of foundation

--- src/main/java/com/citi/foundation/First.java
+++ src/main/java/com/citi/foundation/First.java
@@ -12,7 +12,7 @@ public class First {
      * JDK: Java Development Kit简称， Java开发工具包， 提高Java的开发环境和运行环境.
      * JRK: Java Runtime Environment， Java运行环境， 为Java的运行提供了所需的环境.
      */
-    // ==和equals的区别 
+    // ==和equals的区别
     /*
      * ==基本数据类型： 比较值
      * 引用类型：比较引用是否相同
@@ -44,5 +44,34 @@ public class First {
     public static void mathRound() {
         System.out.println(Math.round(-1.5));
     }
+    /*
+     * 基础数据类型：byte boolean char short long int float double
+     */
 
+    /*
+     * 操作字符串类型
+     * String StringBuffer StringBuilder
+     * String 对象不可变
+     * StringBuffer and StringBuilder 可以操作原有对象
+     * Buffer线程安全， Builder非线程安全
+     */
+    
+    /*
+     * String str = "i"
+     */
+    public static void newString() {
+        // s会被分配到常量池
+        String s = "i";
+        // str会被分配到堆内存
+        String str = new String("i")；
+    }
+
+    /*
+     * 接口和抽象类的区别
+     * 实现： 抽象类的子类由extend继承， 接口必须用implements实现接口
+     * 构造函数： 抽象类可以有构造函数， 接口不能有
+     * main方法：抽象类可以有main方法，接口不能有main方法
+     * 实现数量：类可以实现很多接口，但只能继承一个抽象类
+     * 访问修饰符：接口中方法默认使用public， 抽象类可以任意访问修饰符
+     */
 }
